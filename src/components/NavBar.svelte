5efd7f880d736b948cdc4702a3f7d4654718a2c7
John Huynh <jhnhnyh9@gmail.com>
2021-12-18 22:15:44 -0800

    feature(levels page): added levels page and list

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -2,7 +2,7 @@
 	<title>Bad Captcha Game</title>
 </svelte:head>
 
-<nav class="flex justify-center w-full">
+<nav class="m-4 flex justify-center w-full">
 	<a class="mx-4 text-lg" href={'/'}>Home</a>
 	<a class="mx-4 text-lg" href={'/levels'}>Levels</a>
 </nav>
--- src/routes/levels.svelte
+++ src/routes/levels.svelte
@@ -1,5 +1,41 @@
 <script>
-	//
+	let levels = [
+		{
+			idx: 0,
+			name: 'growing bars'
+		},
+		{
+			idx: 1,
+			name: 'growing bars'
+		},
+		{
+			idx: 2,
+			name: 'growing bars'
+		},
+		{
+			idx: 3,
+			name: 'growing bars'
+		},
+		{
+			idx: 4,
+			name: 'growing bars'
+		},
+		{
+			idx: 5,
+			name: 'growing bars'
+		}
+	];
 </script>
 
-<div>Levels List</div>
+<h1 class="my-8 text-4xl text-center">Levels List</h1>
+<div class="flex justify-center">
+	<div class="w-11/12 rounded-lg shadow-lg lg:w-1/3">
+		<ul class="divide-y-2 divide-gray-100">
+			{#each levels as level (level.idx)}
+				<li class="p-3 hover:bg-zinc-600 hover:text-blue-300">
+					<a href={`/levels/${level.idx}`}>{`Level - ${level.idx}`}</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</div>
