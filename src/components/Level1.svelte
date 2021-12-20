11952919dbe062afb298e3de19adb189fb6cae01
John Huynh <jhnhnyh9@gmail.com>
2021-12-20 13:17:44 -0800

    feature(prompt): added randomize logic to prompt and handle loading.

--- src/components/Level1.svelte
+++ src/components/Level1.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { onMount } from 'svelte';
+
 	export let idx;
 
 	// TODO: turn this into a level page template
@@ -21,6 +23,45 @@
 			keyPresses = [];
 		}
 	};
+
+	const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	const numbers = '0123456789';
+	const combo = letters + numbers;
+
+	const generatePrompt = (length, mode = 'combo') => {
+		let result = '';
+		let characters;
+
+		switch (mode) {
+			case 'letters':
+				characters = letters;
+				break;
+			case 'numbers':
+				characters = numbers;
+				break;
+			case 'combo':
+				characters = combo;
+				break;
+			default:
+				characters = combo;
+				break;
+		}
+
+		const charactersLength = characters.length;
+		for (var i = 0; i < length; i++) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+
+		return result;
+	};
+
+	let promptCode;
+
+	onMount(() => {
+		return (promptCode = generatePrompt(5, 'letters'));
+	});
+
+	// promptCode = generatePrompt(5, 'letters');
 </script>
 
 <svelte:window on:keydown={handleKeyDown} />
@@ -28,9 +69,12 @@
 <h1 class="m-10 text-center text-2xl underline underline-offset-4">Level {idx}</h1>
 <div class="m-5 flex justify-center">
 	<div class="box-content p-4 border-4 rounded-lg">
-		<div class="">
-			<!-- TODO: improve this to randomized -->
-			<h1 class="text-5xl hover:blur-2xl select-none">12345</h1>
+		<div>
+			{#if promptCode}
+				<h1 class="text-5xl hover:blur-2xl select-none">{promptCode}</h1>
+			{:else}
+				<div class="animate-spin">⌛</div>
+			{/if}
 		</div>
 	</div>
 </div>
