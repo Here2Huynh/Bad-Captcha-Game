4de362aaafcde154d8ec63d4988e57e363d06559
John Huynh <jhnhnyh9@gmail.com>
2022-01-06 21:00:26 -0800

    feature(home page): added routing, nav bar, and skeleton pages

---
+++ src/routes/in-construction.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { fade } from 'svelte/transition';
+</script>
+
+<div in:fade={{ delay: 500 }}>
+	<div
+		class="flex p-4 m-10 mt-32 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800"
+		role="alert"
+	>
+		<svg
+			class="inline flex-shrink-0 mr-3 w-5 h-5"
+			fill="currentColor"
+			viewBox="0 0 20 20"
+			xmlns="http://www.w3.org/2000/svg"
+			><path
+				fill-rule="evenodd"
+				d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+				clip-rule="evenodd"
+			/></svg
+		>
+		<div>
+			<span class="font-medium">Hello!</span> This page is under construction 😬. Why don't you
+			check out the <a href={'/levels'} class="font-bold">levels</a>?
+		</div>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,24 +9,34 @@
 </script>
 
 <div in:fade={{ delay: 500 }}>
-	<div
-		class="flex p-4 m-10 mt-32 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800"
-		role="alert"
-	>
-		<svg
-			class="inline flex-shrink-0 mr-3 w-5 h-5"
-			fill="currentColor"
-			viewBox="0 0 20 20"
-			xmlns="http://www.w3.org/2000/svg"
-			><path
-				fill-rule="evenodd"
-				d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-				clip-rule="evenodd"
-			/></svg
-		>
-		<div>
-			<span class="font-medium">Hello!</span> This page is under construction 😬. Why don't you
-			check out the <a href={'/levels'} class="font-bold">levels</a>?
+	<div class="flex justify-center mt-20">
+		<div class="inline-flex rounded-md shadow-sm" role="group">
+			<a href={'/in-construction'}>
+				<button
+					type="button"
+					class="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+				>
+					Join
+				</button>
+			</a>
+
+			<a href={'/login'}>
+				<button
+					type="button"
+					class="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+				>
+					Login
+				</button>
+			</a>
+
+			<a href={'/levels'}>
+				<button
+					type="button"
+					class="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+				>
+					idc
+				</button>
+			</a>
 		</div>
 	</div>
 </div>
